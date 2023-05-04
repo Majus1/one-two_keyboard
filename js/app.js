2c40fdf3c05230173e485424c1312655d1e0fde8
Maj <59331721+Majus1@users.noreply.github.com>
2023-05-04 17:59:45 +0200

    Added an if statement that only allows numbers to be inputed

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 // ::::: Function that types out what is pressed on keyboard
 function displayKeystroke(pressedKey, displayedKeyValue) {
 
-    // test
-    console.log(`This is the pressed key ${pressedKey}. The current number on screen is ${displayedKeyValue}`);
-
-    // ::: Changes the displayed number on screen with the pressed key.
-    displayedKeyValue.innerHTML = `Updated to ${pressedKey}`;
+    // ::: Checks if the typed value is a number or action ::::
+    if (pressedKey === "0" || pressedKey === "1" || pressedKey === "2" || pressedKey === "3" || pressedKey === "4" || pressedKey === "5" || pressedKey === "6" || pressedKey === "7" || pressedKey === "8" || pressedKey === "9") {
+        // Changes the displayed number on screen with the pressed key.
+        displayedKeyValue.innerHTML = `Updated to ${pressedKey}`;
+    }
 
 }
 // ::::: Function that types out what is pressed on keyboard
