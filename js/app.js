e00c7d06c872dce712318c3594cbff678acab754
Maj <59331721+Majus1@users.noreply.github.com>
2023-05-04 18:01:31 +0200

    test

--- js/app.js
+++ js/app.js
@@ -2,10 +2,7 @@
 function displayKeystroke(pressedKey, displayedKeyValue) {
 
     // ::: Checks if the typed value is a number or action ::::
-    if (pressedKey === "0" || pressedKey === "1" || pressedKey === "2" || pressedKey === "3" || pressedKey === "4" || pressedKey === "5" || pressedKey === "6" || pressedKey === "7" || pressedKey === "8" || pressedKey === "9") {
-        // Changes the displayed number on screen with the pressed key.
-        displayedKeyValue.innerHTML = `Updated to ${pressedKey}`;
-    }
+    
 
 }
 // ::::: Function that types out what is pressed on keyboard
