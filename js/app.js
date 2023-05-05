9b32a641b7ff9f2438604cd2159099c1200168a5
Maj <59331721+Majus1@users.noreply.github.com>
2023-05-05 12:06:54 +0200

    Inputed numbers are now displayed

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 // ::::: Function that types out what is pressed on keyboard
-function displayKeystroke(pressedKey, displayedKeyValue) {
+function displayKeystroke(pressedKey, disKeyValue) {
+
+    // Variables
+    let previuslyDisKeyValue = disKeyValue.textContent;
+    let currentDisKeyValue = `${previuslyDisKeyValue}${pressedKey}`;
 
     // ::: Checks if the typed value is a number or action ::::
     if (pressedKey === "0" || pressedKey === "1" || pressedKey === "2" || pressedKey === "3" || pressedKey === "4" || pressedKey === "5" || pressedKey === "6" || pressedKey === "7" || pressedKey === "8" || pressedKey === "9") {
+        
         // Changes the displayed number on screen with the pressed key.
-        displayedKeyValue.innerHTML = `Updated to ${pressedKey}`;
+        disKeyValue.innerHTML = currentDisKeyValue;
+        
     }
     
-}
+};
 // ::::: Function that types out what is pressed on keyboard
 
 // ::::: Event listens for keyboard strokes :::::
@@ -15,8 +21,8 @@ document.addEventListener("keydown", function(target) {
 
     // Stores pressed key
     let pressedKey = target.key;
-    let displayedKeyValue = document.querySelector(".typed-number");
+    let disKeyValue = document.querySelector(".typed-number");
 
-    displayKeystroke(pressedKey, displayedKeyValue)
+    displayKeystroke(pressedKey, disKeyValue)
 });
 // ::::: Event listens for keyboard strokes :::::
